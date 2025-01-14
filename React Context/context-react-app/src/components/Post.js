ef1c2820a5eb1df4896701aab6a5f73d6092bd4f
akhikumar123 <108415318+akhikumar123@users.noreply.github.com>
2025-01-13 20:34:53 -0500

    react Context jan 13

--- React Context/context-react-app/src/components/Post.js
+++ React Context/context-react-app/src/components/Post.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Comment from "./Comment";
+import { useContext } from "react";
+import UserInfoContext from "../context/UserInfoContext";
 
 export default function Post() {
+  const userInfo = useContext(UserInfoContext);
   return (
     <div>
+      {userInfo.isAdmin && <button>Delete</button>}
       <h2>Example Post</h2>
       <p>This is an exaple post content</p>
       <Comment></Comment>
