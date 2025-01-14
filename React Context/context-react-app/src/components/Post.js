971dc6374d014fe133572bf26b561cc5dc5f6da3
akhikumar123 <108415318+akhikumar123@users.noreply.github.com>
2025-01-13 19:26:42 -0500

    Jan 13 React Context

---
+++ React Context/context-react-app/src/components/Post.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Comment from "./Comment";
+
+export default function Post() {
+  return (
+    <div>
+      <h2>Example Post</h2>
+      <p>This is an exaple post content</p>
+      <Comment></Comment>
+    </div>
+  );
+}
