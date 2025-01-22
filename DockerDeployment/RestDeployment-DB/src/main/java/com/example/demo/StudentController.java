f3625ac55174c24ecd4edc652f5abfd38e6b63a0
Akhilesh Kumar <kumara2@miamioh.edu>
2025-01-22 09:32:52 -0500

    Docker file Volumnes

--- DockerDeployment/RestDeployment-DB/src/main/java/com/example/demo/StudentController.java
+++ DockerDeployment/RestDeployment-DB/src/main/java/com/example/demo/StudentController.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,12 +11,16 @@ public class StudentController {
 	@Autowired
 	StudentRepo studentrepo;
 	
-	@RequestMapping("/getStudents")
+	@GetMapping("/getStudents")
 	public List<Student> getStudents() {
-		return List.of(
-				new Student(1, "Akhilesh", 21),
-				new Student(2, "Bob", 22),
-				new Student(3, "Charlie", 23)
-				);
+		return studentrepo.findAll();
+	}
+	
+	@GetMapping("/addStudent")
+	public void addStudent() {
+		Student s = new Student();
+		s.setName("Raj");
+		s.setAge(25);
+		studentrepo.save(s);
 	}
 }
---
+++ DockerDeployment/RestDeployment-DB/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student (age, name) values( 21, 'Akhilesh');
+insert into student (age, name) values(22, 'Bob');
+insert into student (age, name) values( 23, 'Charlie');
+insert into student (age, name) values( 24, 'Don');
